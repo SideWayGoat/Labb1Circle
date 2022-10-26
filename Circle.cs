f47395e04b7608bdc8dcbfb00913af0329e7f760
Patrik Skattberg <patrikskattberg@hotmail.com>
2022-10-26 14:57:40 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Labb1Circle
+{
+    public class Circle
+    {
+        public float pi = 3.141f;
+        public int radius;
+        public Circle(int _Radius)
+        {
+            this.radius = _Radius;
+        }
+
+        public float getArea()
+        {
+            return radius * radius * pi;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Labb1Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle area5 = new Circle(5);
+            Console.WriteLine(area5.getArea());
+
+            Circle area6 = new Circle(6);
+            Console.WriteLine(area6.getArea());
+        }
+    }
+}
